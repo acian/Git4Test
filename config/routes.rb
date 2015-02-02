91e52665d273b35a9b3b7612301a69de10b1d213
Ayelen Cian <cianayelen@gmail.com>
2015-02-01 21:35:08 -0300

    Add Posts

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Apptest::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  resources :posts
+  #root "posts#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150126015512) do
+ActiveRecord::Schema.define(version: 20150201234753) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
